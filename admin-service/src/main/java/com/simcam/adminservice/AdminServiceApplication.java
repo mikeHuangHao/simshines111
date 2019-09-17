1d4aa05630b5e13bc8d3010495cabb2572d76f15
独狼 <2509024314@qq.com>
2019-09-17 10:04:19 +0800

    Merge branch 'master' of https://github.com/javabo1/simshines
    # Conflicts:
    #	.idea/workspace.xml

---
+++ admin-service/src/main/java/com/simcam/adminservice/AdminServiceApplication.java
@@ -0,0 +1,17 @@
+package com.simcam.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hh
+ * 后台管理模块
+ */
+@SpringBootApplication
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+
+}
